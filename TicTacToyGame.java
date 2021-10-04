eea17a358ce84404bd34cf9f63d1329b65d27d97
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-04 10:59:27 +0530

    ADDED: Board_Creation

--- TicTacToyGame.java
+++ TicTacToyGame.java
@@ -1,7 +1,20 @@
 package com.bridgelabz.Tic.Tac.Toy.Game;
 
 public class TicTacToyGame {
+	// Declaring and Assigning Variables As a Globally
+	public static char[] element;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tac Toy Game");
+		// TO call a Method in Main method
+		boardCreation();
+
+	}
+
+	private static void boardCreation() {
+		element = new char[10];
+		for (int i = 0; i < 10; i++) {
+			element[i] = ' ';
+		}
+
 	}
 }
