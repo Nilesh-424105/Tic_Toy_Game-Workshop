c0b17f1a77f55f10204e2f8ecd588681900bcb33
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-11 09:21:33 +0530

    ADDED: Done UC2_Player_Choosing_Option

--- TicTacToyGame.java
+++ TicTacToyGame.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.Tic.Tac.Toy.Game;
 
+import java.util.Scanner;
+
 public class TicTacToyGame {
 	// Declaring and Assigning Variables As a Globally
 	public static char[] element;
+	public static char playerLetter, computerLetter;
 
 	public static void main(String[] args) {
 		// TO call a Method in Main method
 		boardCreation();
+		playerChoose();
 
 	}
 
@@ -16,5 +20,34 @@ public class TicTacToyGame {
 			element[i] = ' ';
 		}
 
+	}
+
+	// Create static method for Board Creation
+	private static void playerChoose() {
+		System.out.println("choose 1 for 'X' choose 2 for 'O' ");
+		Scanner sc = new Scanner(System.in);
+		int option = sc.nextInt();
+
+		// choose option for player
+		switch (option) {
+		case 1:
+			playerLetter = 'X';
+			computerLetter = 'O';
+			System.out.println("player Choosing: " + playerLetter);
+			System.out.println("Computer Choosing: " + computerLetter);
+			break;
+
+		case 2:
+			playerLetter = 'O';
+			computerLetter = 'X';
+			System.out.println("player Choosing: " + playerLetter);
+			System.out.println("Computer Choosing: " + computerLetter);
+			break;
+
+		default:
+			System.out.println("Invalid option");
+			playerChoose();
+
+		}
 	}
 }
